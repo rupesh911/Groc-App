fe5ebd0ae037cb81a2ccd9219ef58359d22d4145
rupesh911 <81091408+rupesh911@users.noreply.github.com>
2026-06-14 21:24:02 +0530

    Move MongoDB connection string to env

--- server/models/mongo.js
+++ server/models/mongo.js
@@ -1,19 +1,38 @@
-const mongoose = require("mongoose")
-const chalk = require("chalk")
-mongoose.connect(`mongodb+srv://rupesh123:Rupesh1299@grocery.rcthdnn.mongodb.net/`, {
+// const mongoose = require("mongoose")
+// const chalk = require("chalk")
+// mongoose.connect(`mongodb+srv://rupesh123:Rupesh1299@grocery.rcthdnn.mongodb.net/`, {
    
-useCreateIndex: true,
+// useCreateIndex: true,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false 
+// });
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//     console.log(chalk.greenBright.bold("Connected to Database Successfully !!!"))
+// })
+
+// // fill: rgb(12 10 9);
+
+// // color: rgba(0, 0, 0, 0.54);
+// //     padding: 4px;
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false 
-});
-
-const connection = mongoose.connection;
-connection.once("open", () => {
-    console.log(chalk.greenBright.bold("Connected to Database Successfully !!!"))
-})
-
-// fill: rgb(12 10 9);
-
-// color: rgba(0, 0, 0, 0.54);
-//     padding: 4px;
+  })
+  .then(() => {
+    console.log(
+      chalk.greenBright.bold("Connected to Database Successfully !!!")
+    );
+  })
+  .catch((err) => {
+    console.log(
+      chalk.redBright.bold("Database Connection Failed !!!"),
+      err.message
+    );
+  });
